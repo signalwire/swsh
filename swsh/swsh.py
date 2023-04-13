3a01fc5d10dca84c81a44ca0cecbde93fa8082c5
Shane Harrell <shane.harrell@signalwire.com>
2023-04-13 15:11:05 -0400

    SWISH-5: Deprecating the ngrok_tunnel command

--- swsh/swsh.py
+++ swsh/swsh.py
@@ -164,20 +164,6 @@ Cross platform command line utility and shell for administering a Space or Space
         '''Return the SWiSH Environment Variables'''
         get_shell_env_all()
 
-    def do_ngrok_tunnel(self, inp):
-        '''Show and Attach to ngrok Tunnel Screen'''
-        tunnel_url = os.popen("curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url'").read()
-        if tunnel_url == "":
-            # The Tunnel isn't active
-            print('There are no active NGROK tunnels established.\n')
-        else:
-            print("NGROK Tunnel URL = " + tunnel_url + "\n")
-            print("The NGROK console runs in SCREEN.  Press ctrl+a ctrl+d to disconnect. ")
-            selection = input("Would you like to connect to the NGROK console? (y/n): ")
-            if selection.lower() == "y" or selection.lower == "yes":
-                os.system("screen -r ngrok")
-                os.system("clear")
-
 ## SIP ENDPOINT COMMAND ##
     # Create the top level parser for sip endpoints: sip_endpoint
     base_sip_endpoint_parser = cmd2.Cmd2ArgumentParser()
